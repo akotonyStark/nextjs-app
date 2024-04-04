e44354111da1f32d60ae0540e4b68754148cddac
akotonystark <alarbiampofo@gmail.com>
2024-04-03 23:43:00 -0400

    fonts, styles and images

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from "next/image";
+
 const Navbar = () => {
     return (
       
         <nav>
+            <Image src={'../../next.svg'} alt='logo' width={80} height={80} placeholder='blur' blurDataURL='https://nextjs.org/'/>
             <h2>Task Manager</h2>
             <Link href="/">Dashboard</Link>
             <Link href="/tickets">Tickets</Link>
