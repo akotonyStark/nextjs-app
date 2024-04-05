7f9557a06738fd1e0f176d1b111dcaf79f595005
akotonystark <alarbiampofo@gmail.com>
2024-04-04 22:46:34 -0400

    Static rendering

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,9 +1,27 @@
+import {notFound} from 'next/navigation'
 import React from 'react'
+
+//what to do when a page hasnt been pre-rendered ahead of time
+export const dynamicParams = true
+
+//cache static data for the pages
+export const generateStaticParams = async() => {
+    const response = await fetch(`http://localhost:4000/tickets`)
+    const tickets = await response.json()
+
+    return tickets.map((ticket) => {
+        return {id: ticket.id}
+    })
+} 
 
 const getData = async (id) => {
     const response = await fetch(`http://localhost:4000/tickets/${id}`, {next: {
         revalidate: 30 //use 0 to opt out of cache
     }})
+
+    if(!response.ok){
+        notFound()
+    }
     return response.json()
 }
 
